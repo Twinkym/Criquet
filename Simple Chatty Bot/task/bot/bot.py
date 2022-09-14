fb815cff55225ba8a96a564c1dad47ce95c3f557
twink <twinkymen@hotmail.com>
2022-09-14 18:10:28 +0200

    First Step of a chatbot in PY. just introductions.

--- Simple Chatty Bot/task/bot/bot.py
+++ Simple Chatty Bot/task/bot/bot.py
@@ -1,4 +1,4 @@
 bot_name = "Criquet"
 birth_year = 2020
 
-print("Bon jour! Je m'appelle {both_name}  \n 'et J'ai été créé en:' {2020}. ")
+print("Bon jour! Je m'appelle {both_name} \n 'et J'ai été créé en:' {2020}. ")
